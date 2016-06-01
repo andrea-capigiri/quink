7ac8a17b7224a37967c7a20a591a4bcc88730ac1
Andrea Capigiri <andreacapigiri@gmail.com>
2016-06-01 08:26:26 +0200

    Implementata visualizzazione cartelle

--- script/main.js
+++ script/main.js
@@ -1,23 +1,54 @@
+$('.goto').click(function() { chrome.tabs.update({ url: $(this).data('url') }); });
+$('body').on('click', '.md-table', function() {
+    var folderID = $(this).data('folder');
+    var subContainer = $('.md-folder-exploded[data-folder="' + folderID + '"]');
+    if (subContainer.is(':visible')) {
+        $(this).find('.md-item-expand').html('<span class="mdi mdi-plus"></span>');
+        subContainer.slideUp(100);
+    } else {
+        $(this).find('.md-item-expand').html('<span class="mdi mdi-minus"></span>');
+        subContainer.slideDown(100);
+    }
+});
+
+var opt = {
+    usefavicon:       true,
+    mostvisited:      true,
+    notepad:          true,
+    autoexpanded:     false
+};
+chrome.storage.sync.get('options', function (data) {
+    /// GETTING OPTIONS FROM STORAGE
+    if (data == null) chrome.storage.sync.set('options', opt);
+    else opt = $.extend({}, opt, data);
+
+    chrome.bookmarks.getSubTree('1', function(items) {
+        $(".bookmark-bar").html(Generate_Bookmark_Item(items[0].children));
+    });
+
+
+
+});
+
+
 ///
 /// ON LOAD
-chrome.storage.sync.get("optShowMostVisited", function(obj) {
+/*chrome.storage.sync.get("optShowMostVisited", function(obj) {
     chrome.topSites.get(function (items) {
         $(".most-visited").html( $(".most-visited").html() +  Generate_MostVisited_Item(items) );
         if(obj.optShowMostVisited) $(".most-visited").show();
     });
 });
-var optUseFavicon = false;
-chrome.storage.sync.get("optUseFavicon", function(obj) { optUseFavicon = obj.optUseFavicon; });
+*/
 
 $(".bookmark-bar").html("");
-chrome.bookmarks.getChildren("1", function(items) {
-    $(".bookmark-bar").html( $(".bookmark-bar").html() + Generate_Bookmark_Item(items) );
-});
 
+
+/*
 $(".other-bookmark").html("");
-chrome.bookmarks.getChildren("2", function(items) {
+chrome.bookmarks.getSubTree("2", function(items) {
     $(".other-bookmark").html( $(".other-bookmark").html() + Generate_Bookmark_Item(items) );
-});
+});*/
 
 ///
 ///   FUNCTIONS
@@ -37,7 +68,7 @@ function Generate_MostVisited_Item(item) {
     else if (item.url != undefined) {
         return "<a href='" + item.url + "'>\
                     <div class='item'>\
-                        <img src='chrome://thumb/" + item.url + "' />\
+                        <img src='' />\
                         <div class='label'>" + item.title + "</div>\
                     </div>\
                 </a>";
@@ -55,32 +86,28 @@ function Generate_Bookmark_Item(item) {
     } /// Bookmark
     else if (item.url != undefined) {
         item.icon = "<span class='mdi mdi-bookmark-outline'>";
-        if (optUseFavicon) item.icon = "<img src='chrome://favicon/" + item.url + "'>";
+        if (opt.usefavicon) item.icon = "<img src='chrome://favicon/" + item.url + "'>";
 
-        return "<a href='" + item.url + "'>\
-                    <div class='md-item'>\
+        return "<a class='md-table' href='" + item.url + "'>\
+                    <div class='md-row'>\
                         <div class='md-item-icon'>" + item.icon + "</div>\
                         <div class='md-item-title'>" + item.title + "</div>\
                     </div>\
                 </a>";
     } /// Cartella
     else {
-        return "<a href='" + item.url + "'>\
-                    <div class='md-item'>\
-                        <div class='md-item-icon'><span class='mdi mdi-folder-outline'></div>\
+        item.expandicon = "<span class='mdi mdi-plus'></span>";
+        item.expanddisplay = "style='display:none;'";
+        if (opt.autoexpanded) { item.expandicon = "<span class='mdi mdi-minus'></span>"; item.expanddisplay = ""; }
+
+        return "<div class='md-table' data-folder='" + item.id + "'>\
+                    <div class='md-row md-folder'>\
                         <div class='md-item-title'>" + item.title + "</div>\
+                        <div class='md-item-expand'>" + item.expandicon + "</div>\
                     </div>\
-                </a>"
-                + Generate_Bookmark_Item(item.children);
-        return a;
+                </div>\
+                <div class='md-folder-exploded' data-folder='" + item.id + "' " + item.expanddisplay + " >\
+                    " + Generate_Bookmark_Item(item.children) + "\
+                </div>";
     }
 }
-
-$("main").masonry({
-    itemSelector: ".flex-item",
-    columnWidth: ".flex-item",
-    gutter: 10,
-    fitWidth: true
-});
-
-$(".goto").click(function() { chrome.tabs.update({ url: $(this).data("url") }); });
--- script/options.js
+++ script/options.js
@@ -1,17 +1,30 @@
 
-chrome.storage.sync.get("optShowMostVisited", function(obj) { $("[name='optShowMostVisited']").prop("checked", obj.optShowMostVisited); });
-chrome.storage.sync.get("optUseFavicon", function(obj) { $("[name='optUseFavicon']").prop("checked", obj.optUseFavicon); });
+var opt = [
+    { 'use-favicon':        true  },
+    { 'show-mostvisited':   false },
+    { 'show-notepad':       false }];
 
-$("input[name='optShowMostVisited']").change(function() {
-    var data = {};
-    data.optShowMostVisited = $("[name='optShowMostVisited']").prop("checked");
-    chrome.storage.sync.set(data, Alert);
+
+chrome.storage.sync.get(null, function(items) {
+    var allKeys = Object.keys(items);
+    console.log(allKeys);
+    for (var i=0; i<Object.keys(items); i++)
+    if (opt.item) {
+
+    }
 });
-$("input[name='optUseFavicon']").change(function() {
-    var data = {};
-    data.optUseFavicon = $("[name='optUseFavicon']").prop("checked");
-    chrome.storage.sync.set(data, Alert());
-});
+
+
+
+
+
+chrome.storage.sync.get("opt_autoexpanded", function(obj) { $("[name='optAutoExpanded']").prop("checked", obj.opt_autoexpanded); });
+chrome.storage.sync.get("opt_usefavicon", function(obj) { $("[name='optUseFavicon']").prop("checked", obj.opt_usefavicon); });
+
+$("checkbox").change(function() { chrome.storage.sync.set({ 'opt_showmostvisited': $("[name='optShowMostVisited']").prop("checked") }, Alert()); });
+$("checkbox").change(function() { chrome.storage.sync.set({ 'opt_autoexpanded': $("[name='optAutoExpanded']").prop("checked") }, Alert()); });
+$("checkbox").change(function() { chrome.storage.sync.set({ 'opt_usefavicon': $("[name='optUseFavicon']").prop("checked") }, Alert()); });
+//$("checkbox").change(function() { chrome.storage.sync.set({ 'opt_showmostvisited': $("[name='optShowMostVisited']").prop("checked") }, Alert()); });
 
 $(".goto").click(function() { chrome.tabs.update({ url: $(this).data("url") }); });
 function Alert() { $(".popup").fadeIn(); setTimeout(function() { $(".popup").fadeOut(); }, 750); }
